7caad845a72a52af698485eaa20102d72bd058aa
Max <max.graf@live.at>
2021-11-05 18:33:20 +0000

    Integrate hand pose cubes into mapping plane -> makes GestureSelectionMenu component obsolete

--- Source/CoCreationUE4_27/Private/MyCPPBlueprintFunctionLibrary.cpp
+++ Source/CoCreationUE4_27/Private/MyCPPBlueprintFunctionLibrary.cpp
@@ -2,3 +2,17 @@
 
 
 #include "MyCPPBlueprintFunctionLibrary.h"
+
+TArray<int> UMyCPPBlueprintFunctionLibrary::singleNoteMap = {0};
+TArray<int> UMyCPPBlueprintFunctionLibrary::majMap = {0, 4, 7};
+TArray<int> UMyCPPBlueprintFunctionLibrary::minMap = {0, 3, 7};
+TArray<int> UMyCPPBlueprintFunctionLibrary::dimMap = {0, 4, 6};
+TArray<int> UMyCPPBlueprintFunctionLibrary::augMap = {0, 4, 8};
+TArray<int> UMyCPPBlueprintFunctionLibrary::domMap = {0, 4, 7, 10};
+TArray<int> UMyCPPBlueprintFunctionLibrary::maj7Map = {0, 4, 7, 11};
+TArray<int> UMyCPPBlueprintFunctionLibrary::min7Map = {0, 3, 7, 10};
+TArray<int> UMyCPPBlueprintFunctionLibrary::d7Map = {0, 3, 6, 9};
+TArray<int> UMyCPPBlueprintFunctionLibrary::hd7Map = {0, 3, 6, 10};
+TArray<int> UMyCPPBlueprintFunctionLibrary::sus2Map = {0, 2, 7};
+TArray<int> UMyCPPBlueprintFunctionLibrary::sus4Map = {0, 5, 7};
+
--- Source/CoCreationUE4_27/Public/MyCPPBlueprintFunctionLibrary.h
+++ Source/CoCreationUE4_27/Public/MyCPPBlueprintFunctionLibrary.h
@@ -12,6 +12,19 @@
 
 using namespace std;
 
+UENUM(BlueprintType)
+enum class EChord : uint8 {
+	singleNote			UMETA(DisplayName="single note"),
+	maj					UMETA(DisplayName="major"),
+	min					UMETA(DisplayName="minor"),
+	dim					UMETA(DisplayName="diminished"),
+	aug					UMETA(DisplayName="augmented"),
+	dom					UMETA(DisplayName="dominant"),
+	maj7				UMETA(DisplayName="major 7th"),
+	min7				UMETA(DisplayName="minor 7th"),
+	d7					UMETA(DisplayName="diminished 7th"),
+	hd7					UMETA(DisplayName="half-diminished 7th"),
+};
 
 /**
  * 
@@ -19,6 +32,7 @@ using namespace std;
 UCLASS()
 class COCREATIONUE4_27_API UMyCPPBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
+	
 	GENERATED_BODY()
 	UFUNCTION(BlueprintCallable, Category = "MyCPPLibrary")
 	static FQuat GetQuatFromRotator(FRotator rotator)
@@ -31,4 +45,51 @@ class COCREATIONUE4_27_API UMyCPPBlueprintFunctionLibrary : public UBlueprintFun
     {
         return modf(InValue, &OutIntPart);
     }
+
+	/* Chord maps (defined in cpp file) */
+	static TArray<int> singleNoteMap;
+	static TArray<int> majMap;
+	static TArray<int> minMap;
+	static TArray<int> dimMap;
+	static TArray<int> augMap;
+	static TArray<int> domMap;
+	static TArray<int> maj7Map;
+	static TArray<int> min7Map;
+	static TArray<int> d7Map;
+	static TArray<int> hd7Map;
+	static TArray<int> sus2Map;
+	static TArray<int> sus4Map;
+	
+    
+    UFUNCTION(BlueprintCallable, Category = "MyCPPLibrary")
+    static TArray<int> GetChordMap(EChord chord)
+	{
+		switch(chord)
+		{
+		case EChord::singleNote:
+			return singleNoteMap;
+		case EChord::maj:
+			return majMap;
+		case EChord::min:
+			return minMap;
+		case EChord::dim:
+			return dimMap;
+		case EChord::aug:
+			return augMap;
+		case EChord::dom:
+			return domMap;
+		case EChord::maj7:
+			return maj7Map;
+		case EChord::min7:
+			return min7Map;
+		case EChord::d7:
+			return d7Map;
+		case EChord::hd7:
+			return hd7Map;
+		default:
+			return singleNoteMap;
+		}
+	}
+
 };
+
